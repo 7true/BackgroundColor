d40b3f366146235861e26970c25d6277aa130546
alx <37true@gmail.com>
2018-11-17 23:52:59 +0700

    common onClick()

--- app/src/main/java/tk/alltrue/colorbuttons/MainActivity.java
+++ app/src/main/java/tk/alltrue/colorbuttons/MainActivity.java
@@ -1,14 +1,16 @@
 package tk.alltrue.colorbuttons;
 
+import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
 
     private ConstraintLayout mConstraintLayout;
     private TextView mInfoTextView;
@@ -20,24 +22,50 @@ public class MainActivity extends AppCompatActivity {
         mConstraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
         mInfoTextView = (TextView) findViewById(R.id.textViewInfo);
 
-        Button yellowButton = (Button) findViewById(R.id.buttonYellow);
+        final Button yellowButton = (Button) findViewById(R.id.buttonYellow);
+        final Button redButton = (Button) findViewById(R.id.buttonRed);
+        final Button greenButton = (Button) findViewById(R.id.buttonGreen);
 
-        yellowButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        yellowButton.setOnClickListener(this);
+        redButton.setOnClickListener(this);
+        greenButton.setOnClickListener(this);
+
+//        yellowButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mInfoTextView.setText(R.string.yellow);
+//                mConstraintLayout.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.yellow));
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonYellow:
                 mInfoTextView.setText(R.string.yellow);
                 mConstraintLayout.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.yellow));
-            }
-        });
+                break;
+            case R.id.buttonRed:
+                mInfoTextView.setText(R.string.red);
+                mConstraintLayout.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.red));
+                break;
+            case R.id.buttonGreen:
+                mInfoTextView.setText(R.string.green);
+                mConstraintLayout.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+                break;
+        }
     }
 
-    public void onRedButtonClick(View view) {
-        mInfoTextView.setText(R.string.red);
-        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.red));
-    }
 
-    public void onGreenButtonClick(View view) {
-        mInfoTextView.setText(R.string.green);
-        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.green));
-    }
+//    public void onRedButtonClick(View view) {
+//        mInfoTextView.setText(R.string.red);
+//        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.red));
+//    }
+//
+//    public void onGreenButtonClick(View view) {
+//        mInfoTextView.setText(R.string.green);
+//        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.green));
+//    }
+
 }
